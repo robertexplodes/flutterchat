195d7f052eb557d7a51545d762b2127aa5bcbd44
KainFlorian <flo.kainrath@gmail.com>
2022-03-23 15:48:33 +0100

    added secure key storage

--- lib/domain/login_provider.dart
+++ lib/domain/login_provider.dart
@@ -1,10 +1,23 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
 
 class LoginProvider with ChangeNotifier {
 
+  final storage = const FlutterSecureStorage();
+  final String _baseURL = 'https://identitytoolkit.googleapis.com/v1/';
+  final String _loginURL = 'accounts:signInWithPassword';
+  // https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC_slH-xMuJHpfhjOCG6q8lCDoBdPZyAXI
+
+  Future<String> loadToken() async {
+    return "";
+  }
 
   Future<void> login(String email, String password) async {
+    var response = await http.post(
+        Uri.parse('$_baseURL/$_loginURL?key=\$API_KEY'));
+
     throw UnsupportedError('Not implemented');
   }
 
